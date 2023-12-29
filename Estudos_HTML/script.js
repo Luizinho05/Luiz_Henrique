ba14ecfec086e6da9400a7a94c1875570cdec110
luiz <luizhsimionato@gmail.com>
2023-12-29 09:50:13 -0300

    a

--- Estudos_HTML/script.js
+++
@@ -1,7 +0,0 @@
-const input = document.querySelector('input')
-
-function copieOnumero(){
-    navigator.clipboard.writeText(input.value).then(() => {
-        alert('Copiado para área de Transferência!')
-    })
-}
